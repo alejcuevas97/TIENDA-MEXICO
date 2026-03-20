b6d7858442d4b29e433faee302be93ed7901a434
cesar enrique munoz magdaleno <cesare@estudiantes.uci.cu>
2026-03-20 11:29:18 -0400

    first commit

---
+++ front-tienda/src/app/page.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+export default function Home() {
+  return (
+    <div className="flex flex-col items-center justify-center min-h-screen py-2">
+      <h1 className="text-4xl font-bold mb-4">
+        Bienvenido a la Tienda en México
+      </h1>
+      <p className="text-lg text-gray-600">
+        Explora nuestros productos y disfruta de una experiencia de compra
+        única.
+      </p>
+    </div>
+  );
+}
