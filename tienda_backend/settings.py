d552bef498a56adb3abb8b0d01a5cdbfc9d3f2de
alejcuevas97 <alejcuevas97@gmail.com>
2026-03-29 21:18:09 -0400

    ajustes

--- tienda_backend/settings.py
+++ tienda_backend/settings.py
@@ -2,7 +2,7 @@ import os
 from pathlib import Path
 from datetime import timedelta
 import environ
-
+import dj_database_url
 
 #se utilizapara gregar .env
 env=environ.Env()
@@ -81,13 +81,14 @@ WSGI_APPLICATION = 'tienda_backend.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/6.0/ref/settings/#databases
+DATABASES_URL=os.getenv('DATABASE_URL')
 
 DATABASES = {
-    #'default': {
-     #   'ENGINE': 'django.db.backends.sqlite3',
-      #  'NAME': BASE_DIR / 'db.sqlite3',
+   # 'default': {
+    #    'ENGINE': 'django.db.backends.sqlite3',
+     #   'NAME': BASE_DIR / 'db.sqlite3',
     #}
-    'default': env.db('DATABASE_URL')
+    'default':dj_database_url.parse('DATABASE_URL', conn_max_age=600, ssl_require=True)
 }
 
 
