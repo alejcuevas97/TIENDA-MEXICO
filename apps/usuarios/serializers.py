66adeae32c613084a7768a7646a5eaf15a3fa8bd
alejcuevas97 <alejcuevas97@gmail.com>
2026-05-03 12:25:24 -0400

    ajustes

--- apps/usuarios/serializers.py
+++ apps/usuarios/serializers.py
@@ -5,4 +5,4 @@ from django.contrib.auth.models import User
 class UserAuthSerializers(ModelSerializer):
     class Meta:
         model= User
-        fields=['id','username','email','password']
+        fields=['id','username','email']
--- tienda_backend/settings.py
+++ tienda_backend/settings.py
@@ -83,15 +83,16 @@ WSGI_APPLICATION = 'tienda_backend.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/6.0/ref/settings/#databases
-DATABASES_URL=os.getenv('DATABASE_URL')
 
-DATABASES = {
-   
+DATABASES_URL = {
+   #'default': {
+     #   'ENGINE': 'django.db.backends.sqlite3',
+      #  'NAME': BASE_DIR / 'db.sqlite3',
     'default':
         dj_database_url.config('DATABASE_URL',conn_max_age=600,ssl_require=True)
-
+   }
     
-}
+
 
 
 
